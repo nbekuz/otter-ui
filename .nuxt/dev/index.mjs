755ebaeff56e42c2836c5fc476827946178142c7
NbekWeb <nbekdev@gmail.com>
2026-06-25 13:36:36 +0500

    s

--- .nuxt/dev/index.mjs
+++ .nuxt/dev/index.mjs
@@ -2023,7 +2023,7 @@ async function errorHandler(error, event) {
 
 const rootDir = "/Users/nodirbek/Desktop/otter/otter-app";
 
-const appHead = {"meta":[{"charset":"utf-8"},{"name":"viewport","content":"width=device-width, initial-scale=1, maximum-scale=1"},{"name":"description","content":"ОТТЕР - умный планировщик задач"},{"name":"theme-color","content":"#21A038"}],"link":[{"rel":"preconnect","href":"https://fonts.googleapis.com"},{"rel":"stylesheet","href":"https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"}],"style":[],"script":[],"noscript":[],"title":"ОТТЕР - Планировщик задач"};
+const appHead = {"meta":[{"charset":"utf-8"},{"name":"viewport","content":"width=device-width, initial-scale=1, maximum-scale=1"},{"name":"description","content":"ОТТЕР - умный планировщик задач"},{"name":"theme-color","content":"#21A038"}],"link":[{"rel":"preconnect","href":"https://fonts.googleapis.com"},{"rel":"stylesheet","href":"https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"},{"rel":"icon","type":"image/svg+xml","href":"/favicon.svg"}],"style":[],"script":[],"noscript":[],"title":"ОТТЕР - Планировщик задач"};
 
 const appRootTag = "div";
 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -50,6 +50,7 @@ export default defineNuxtConfig({
       link: [
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
       ],
     },
   },
